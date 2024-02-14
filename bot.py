ae05172ce1b35f39abb5775873a05fe3fc64d9a1
Aastik <aastikdude7@gmail.com>
2024-02-15 04:02:25 +0530

    adding dotnev support and horoscope request

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
 import os 
 import telebot
+import requests
+
+load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -10,3 +14,13 @@ def send_welcome(message):
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
+
+bot.infinity_polling()
+
+def get_daily_horoscope(sign: str, day: str) -> dict:
+    
+    url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
+    params = {"sign": sign, "day": day}
+    response = requests.get(url, params)
+
+    return response.json()
