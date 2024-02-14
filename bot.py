129f4d1d5c07b05cb6ed7db333981c5f1bbf2425
Aastik <aastikdude7@gmail.com>
2024-02-15 04:03:26 +0530

    Message handler for horoscope

--- bot.py
+++ bot.py
@@ -24,3 +24,9 @@ def get_daily_horoscope(sign: str, day: str) -> dict:
     response = requests.get(url, params)
 
     return response.json()
+
+@bot.message_handler(commands=['horoscope'])
+def sign_handler(message):
+    text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, day_handler)
