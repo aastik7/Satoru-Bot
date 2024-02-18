adc4a800d44af7d3e2fa386a28d57ebaa1cd0d7c
Aastik <18290528+aastik7@users.noreply.github.com>
2024-02-18 06:15:42 +0530

    Fixing day handler

--- bot.py
+++ bot.py
@@ -24,19 +24,22 @@ def day_handler(message):
     sign = message.text
     print(f"Received zodiac sign: {sign}")
     text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
-    sent_msg = bot.send_message(
-        message.chat.id, text, parse_mode="Markdown")
-    bot.register_next_step_handler(
-        sent_msg, fetch_horoscope, sign.capitalize())
-    
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    # Here, we use lambda function to pass both sign and message
+    bot.register_next_step_handler(sent_msg, lambda msg: fetch_horoscope(msg, sign.capitalize()))
+
 def fetch_horoscope(message, sign):
     day = message.text
     print(f"Received day: {day}")
     horoscope = get_daily_horoscope(sign, day)
-    data = horoscope["data"]
-    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\n*Sign:* {sign}\n*Day:* {data["date"]}'
-    bot.send_message(message.chat.id, "Here's your horoscope!")
-    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+    if horoscope.get("error"):
+        bot.send_message(message.chat.id, "Sorry, I couldn't fetch the horoscope for that day.")
+    else:
+        data = horoscope["data"]
+        horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\n*Sign:* {sign}\n*Day:* {data["date"]}'
+        bot.send_message(message.chat.id, "Here's your horoscope!")
+        bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+
 
 def get_daily_horoscope(sign: str, day: str) -> dict:
     url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
